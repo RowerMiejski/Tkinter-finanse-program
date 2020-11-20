d28c1b0af1041cdcfe6a69f437df2648d47f94e6
RowerMiejski <rowerkuby123@gmail.com>
2020-11-20 11:30:58 +0100

    Database working with adding new rows

--- Pages.py
+++ Pages.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from tkinter import ttk
+import sqlite3
 
 class MainPage(tk.Frame):
 
@@ -17,6 +18,14 @@ class MainPage(tk.Frame):
         button2.pack()
         myData = tk.LabelFrame(self, text = "Wydatki, dochody")
         myData.pack(fill = "both", expand = "yes", padx=20,pady=10)
+        trv = ttk.Treeview(myData, columns = (1,2,3,4), show = "headings", height = "6")
+        trv.pack()
+        trv.heading(1, text= "Wartość")
+        trv.heading(2, text= "Tytuł")
+        trv.heading(3, text= "Kategoria")
+        trv.heading(4, text= "Data")
+        self.db = sqlite3.connect("db.db")
+        self.cursor = self.db.cursor()
 
     def packNewChanges(self):
         pass
@@ -27,12 +36,13 @@ class Expenses(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-
+        self.db = self.controller.connectDataBase()
+        self.cursor = self.db.cursor()
         #new expenses data
-        self.newExpenseCategory = tk.IntVar()
+        self.newExpenseCategory = tk.StringVar()
         self.newExpenseName = tk.StringVar()
         self.newExpenseDate = tk.StringVar()
-
+        self.newExpensePrice = tk.IntVar()
 
         #looks
         label = tk.Label(self, text="Dodaj Finanse", font=controller.title_font)
@@ -41,13 +51,14 @@ class Expenses(tk.Frame):
                            command=lambda: controller.showFrame("MainPage"))
         expenseButton = tk.Button(self, text="Dodaj nowy wydatek: ",
                            command=self.popupWindow)
-        expenseShowLabel = tk.Label(self, textvariable = self.newExpenseName)
+        expenseShowLabel = tk.Label(self, textvariable = self.newExpensePrice)
         button.pack()
         expenseButton.pack()
         expenseShowLabel.pack()
 
     def addExpense(self):
-        self.controller.showFrame("MainPage")
+        self.cursor.execute("INSERT INTO expenses VALUES (?,?,?,?)", (self.newExpensePrice.get(), self.newExpenseName.get(), self.newExpenseCategory.get(), self.newExpenseDate.get()))
+        self.db.commit()
 
     def addIncome(self):
         pass
@@ -56,12 +67,13 @@ class Expenses(tk.Frame):
         pass
 
     def popupWindow(self):
-        EntryForm(self, self.newExpenseName, self.newExpenseCategory, self.newExpenseDate)
+        EntryForm(self,self.newExpensePrice, self.newExpenseName, self.newExpenseCategory, self.newExpenseDate)
 
 
 class Settings(tk.Frame):
 
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Ustawienia", font=controller.title_font)
@@ -73,24 +85,47 @@ class Settings(tk.Frame):
 
 
 class EntryForm(tk.Toplevel):
-    def __init__(self, master, exName, exCat, exDate):
+    def __init__(self, master,exPrice, exName, exCat, exDate):
         super().__init__(master)
+        self.dict = {}
+        self.dict = self.master.controller.getCategories()
         #recalls
         self.exName = exName
         self.exCat = exCat
         self.exDate = exDate
-
+        self.exPrice = exPrice
         self.geometry("300x300")
-        label = tk.Label(self, text="Podaj nazwe wydatku: ")
-        label.pack()
+        #looks
+        self.getEntries()
+        tk.Button(self, text="submit", command=self.submit).pack()
+
+    def getEntries(self):
+        nameLabel = tk.Label(self, text="Podaj nazwe wydatku: ")
+        nameLabel.pack()
         self.nameEntry = tk.Entry(self)
         self.nameEntry.pack()
 
-        tk.Button(self, text="submit", command=self.submit).pack()
+        costLabel = tk.Label(self, text="Podaj koszt wydatku: ")
+        costLabel.pack()
+        self.costEntry = tk.Entry(self)
+        self.costEntry.pack()
 
+        catLabel = tk.Label(self, text="Podaj kategorie wydatku: ")
+        catLabel.pack()
+        self.catEntry = ttk.Combobox(self, textvariable = self.exCat)
+        self.catEntry['values'] = list(self.dict.values())
+        self.catEntry.current(0)
+        self.catEntry.pack()
+
+        dateLabel = tk.Label(self, text="Podaj date wydatku: ")
+        dateLabel.pack()
+        self.dateEntry = tk.Entry(self)
+        self.dateEntry.pack()
 
     def submit(self):
-        name = self.nameEntry.get()
-        print(name)
-        self.exName.set(name)
+        self.exName.set(self.nameEntry.get())
+        self.exPrice.set(self.costEntry.get())
+        self.exCat.set(self.catEntry.get())
+        self.exDate.set(self.dateEntry.get())
+        self.master.addExpense()
         self.destroy()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import font as tkfont
 from Pages import *
+import sqlite3
 
 
 class Faktury(tk.Tk):
@@ -8,9 +9,9 @@ class Faktury(tk.Tk):
     def __init__(self, *args, **kwargs):
         self.frames = {}
         tk.Tk.__init__(self, *args, **kwargs)
-        self.geometry("800x600")
+        self.geometry("1280x720")
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold")
-
+        self.categories = {}
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -25,6 +26,9 @@ class Faktury(tk.Tk):
 
         self.showFrame("MainPage")
 
+        self.connectDataBase()
+        self.getCategories()
+
     def showFrame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
@@ -34,6 +38,32 @@ class Faktury(tk.Tk):
 
     def addIncome(self):
         pass
+
+    def connectDataBase(self):
+        self.mydb = sqlite3.connect("db.db")
+        self.cursor = self.mydb.cursor()
+        self.cursor.execute('''CREATE TABLE if not exists expenses 
+                               (price real, title text, category integer, date text)''')
+
+        self.cursor.execute('''CREATE TABLE if not exists categories 
+                                       (cat text)''')
+
+        self.cursor.execute('SELECT * FROM categories')
+        try:
+            self.cursor.fetchall()[0][0]
+        except:
+            self.cursor.execute("INSERT INTO categories VALUES ('1-none')")
+        self.cursor.execute('SELECT * FROM expenses')
+        self.mydb.commit()
+        return self.mydb
+
+    def getCategories(self):
+        self.cursor.execute('SELECT * FROM categories')
+        categories = self.cursor.fetchall()
+        for item in categories:
+            names = item[0].split("-")
+            self.categories[names[0]] = names[1]
+        return self.categories
 
 
 if __name__ == "__main__":
